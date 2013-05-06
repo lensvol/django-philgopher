855057b11b6681d004b1b99843c135c0e71381c4
Kirill Borisov <lensvol@gmail.com>
2013-05-07 00:49:10 +0400

    Added support for user specified interface binding; hostname and port are now correctly identified and specified in output to user.

--- philgopher/management/commands/rungopherrun.py
+++ philgopher/management/commands/rungopherrun.py
@@ -11,7 +11,13 @@ class Command(BaseCommand):
     args = ''
     help = 'Sample implementation of Gopher protocol just for fun.'
 
-    option_list = BaseCommand.option_list + ()
+    option_list = BaseCommand.option_list + (
+        make_option('--bind-addr',
+            dest='bind_addr',
+            default=None,
+            help='Bind at specified interface.'
+        ),
+    )
 
     def dump_model_record(self, record):
         return u'\r\n\r\n'.join([u'%s:\r\n%s' %
@@ -21,12 +27,20 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         model_mapping = {}
 
+        # Prepare a list of all models with specified verbose_name and
+        # create a mapping for future reference
         for model_cls in models.get_models():
             if isinstance(model_cls._meta.verbose_name, basestring):
                 model_mapping[model_cls.__name__] = model_cls
 
+        # By default, bind us to all and any network interfaces on this machine
         local_hostname = '0.0.0.0'
         bind_port = 70
+
+        # But if user tells us otherwise, who are we to argue?
+        if options['bind_addr']:
+            (local_hostname, bind_port) = options['bind_addr'].split(':')
+            bind_port = int(bind_port)
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind((local_hostname, bind_port))
@@ -37,6 +51,9 @@ class Command(BaseCommand):
 
         while True:
             (clientsock, address) = sock.accept()
+            # We'll need this to tell client correct hostname for future
+            # requests
+            our_addr = clientsock.getsockname()
 
             print 'Accepted connection from %s:%i!' % address
             chunk = clientsock.recv(255)
@@ -45,8 +62,10 @@ class Command(BaseCommand):
             print 'Received reference: %s' % chunk
             if chunk == '/' or not chunk:
                 reply = '1' + u'\n1'.join(
-                        u'\t'.join((m._meta.verbose_name, '/%s' % m.__name__, '127.0.0.1', '70'))
-                        for m in sorted(model_mapping.values(), key=lambda x: x._meta.verbose_name))
+                        u'\t'.join((m._meta.verbose_name, '/%s' % m.__name__,
+                                    our_addr[0], unicode(our_addr[1])))
+                        for m in sorted(model_mapping.values(),
+                                        key=lambda x: x._meta.verbose_name))
             else:
                 parts = chunk[1:].split('/')
                 if len(parts) == 1:
@@ -55,7 +74,7 @@ class Command(BaseCommand):
 
                     info = u'iModel: "%s" Number of records: %i\tfake\t(NULL)\t0\r\n0' % (model._meta.verbose_name, count)
 
-                    reply = info + u'\n0'.join(u'\t'.join((unicode(rec), '/%s/%i' % (parts[0], rec.id), local_hostname, unicode(bind_port))) for rec in model.objects.all())
+                    reply = info + u'\n0'.join(u'\t'.join((unicode(rec), '/%s/%i' % (parts[0], rec.id), our_addr[0], unicode(our_addr[1]))) for rec in model.objects.all())
                 else:
                     model = model_mapping[parts[0]]
                     rec_id = int(parts[1])
